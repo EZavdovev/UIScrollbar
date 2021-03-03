08c40f742161540c8338e7b0787dff5c184cf4fa
Evgenii <jekaweek1@gmail.com>
2021-03-03 05:10:49 +0200

    Add Events Script

---
+++ UIScrollbar/Assets/Scripts/Events/EventDispatcher.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events {
+    public class EventDispatcher : MonoBehaviour {
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public void Dispatch() {
+            _someEvent.Dispatch();
+        }
+    }
+}
+
