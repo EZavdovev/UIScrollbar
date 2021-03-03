0e4c51e311b2903c62e78edae95d599d41630a4b
Evgenii <jekaweek1@gmail.com>
2021-03-03 05:13:16 +0200

    add Information scripts

---
+++ UIScrollbar/Assets/Scripts/Information/Json/ProductInfoJson.cs
@@ -0,0 +1,10 @@
+[System.Serializable]
+public class ProductInfoJson {
+    public string _nameProduct;
+    public int _iconProductID;
+    public string _countProduct;
+    public string _costProduct;
+    public int _iconAvatarID;
+    public string _lvlAvatar;
+    public string _nameAvatar;
+}
---
+++ UIScrollbar/Assets/Scripts/Information/ProductInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ProductInfo: MonoBehaviour{
+    public TMP_Text _nameProduct;
+    public Image _iconProduct;
+    public TMP_Text _countProduct;
+    public TMP_Text _costProduct;
+    public Image _iconAvatar;
+    public TMP_Text _lvlAvatar;
+    public TMP_Text _nameAvatar;
+}
